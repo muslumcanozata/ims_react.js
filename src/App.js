7757219dcf5bde7cd35e1a4370990bdfee685a20
Müslüm Can <muslumcanozata@gmail.com>
2021-05-18 00:58:53 +0300

    Authentication entegrasyonu tamamlandı. Login ve Signup fonksiyonları sorunsuz bir şekilde çalıştırıldı.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,212 @@
-import React from 'react'
+// import React, { Component } from 'react';
+// import Nav from './components/Nav';
+// import LoginForm from './components/LoginForm';
+// import SignupForm from './components/SignupForm';
+// import axios from 'axios'
+// class App extends Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       displayed_form: '',
+//       logged_in: localStorage.getItem('token') ? true : false,
+//       username: ''
+//     };
+//   }
 
-const App = () => {
-    return (
-        <div>
-            Hello React
-        </div>
-    )
+//   componentDidMount() {
+//     if (this.state.logged_in) {
+//       axios('http://localhost:8000/api/current_user/', {
+//         headers: {
+//           Authorization: `JWT ${localStorage.getItem('token')}`
+//         }
+//       })
+//         .then(res => res.json())
+//         .then(json => {
+//           this.setState({ username: json.username });
+//         });
+//     }
+//   }
+
+//   handle_login = (e, data) => {
+//     e.preventDefault();
+//     axios('http://localhost:8000/token-auth/', {
+//       method: 'POST',
+//       headers: {
+//         'Content-Type': 'application/json'
+//       },
+//       body: JSON.stringify(data)
+//     })
+//       .then(res => res.json())
+//       .then(json => {
+//         localStorage.setItem('token', json.token);
+//         this.setState({
+//           logged_in: true,
+//           displayed_form: '',
+//           username: json.user.username
+//         });
+//       });
+//   };
+
+//   handle_signup = (e, data) => {
+//     e.preventDefault();
+//     axios('http://localhost:8000/api/users/', {
+//       method: 'POST',
+//       headers: {
+//         'Content-Type': 'application/json'
+//       },
+//       body: JSON.stringify(data)
+//     })
+//       .then(res => res.json())
+//       .then(json => {
+//         localStorage.setItem('token', json.token);
+//         this.setState({
+//           logged_in: true,
+//           displayed_form: '',
+//           username: json.username
+//         });
+//       });
+//   };
+
+//   handle_logout = () => {
+//     localStorage.removeItem('token');
+//     this.setState({ logged_in: false, username: '' });
+//   };
+
+//   display_form = form => {
+//     this.setState({
+//       displayed_form: form
+//     });
+//   };
+
+//   render() {
+//     let form;
+//     switch (this.state.displayed_form) {
+//       case 'login':
+//         form = <LoginForm handle_login={this.handle_login} />;
+//         break;
+//       case 'signup':
+//         form = <SignupForm handle_signup={this.handle_signup} />;
+//         break;
+//       default:
+//         form = null;
+//     }
+
+//     return (
+//       <div className="App">
+//         <Nav
+//           logged_in={this.state.logged_in}
+//           display_form={this.display_form}
+//           handle_logout={this.handle_logout}
+//         />
+//         {form}
+//         <h3>
+//           {this.state.logged_in
+//             soruişareti `Hello, ${this.state.username}`
+//             : 'Please Log In'}
+//         </h3>
+//       </div>
+//     );
+//   }
+// }
+
+// export default App;
+
+import React, { Component } from 'react';
+import NavComponent from './components/NavComponent';
+
+class App extends Component {
+	constructor(props) {
+		super(props)
+
+		this.state = {
+			 logged_in : localStorage.getItem('token') ? true : false,
+			 username : '',
+			 displayed_form : ''
+		}
+	}
+
+	componentDidMount(){
+		if(this.state.logged_in){
+			fetch('http://localhost:8000/api/current_user/', {
+				method : 'GET',
+				headers : {
+					Authorization : `JWT ${localStorage.getItem('token')}`
+				}
+			})
+			.then(res => res.json())
+			.then(resp => {
+				this.setState({ username : resp.username })
+			})
+			.catch(err => console.log(err));
+		}
+	}
+
+	display_form = (formName) => {
+        this.setState({
+            displayed_form : formName
+        });
+    }
+
+	handleLoginChange = event => {
+        this.setState({
+            [event.target.name] : event.target.value
+        })
+	}
+	
+	handleLogout = () => {
+		localStorage.removeItem('token');
+		this.setState({logged_in : false, username : ''})
+	}
+
+	handleLogin = (e, data) => {
+		e.preventDefault();
+		console.log(data)
+		fetch('http://localhost:8000/token-auth/', {
+			crossDomain : true,
+			withCredentials : true,
+			async : true,
+			method : 'POST',
+			headers : {
+				'Content-Type' : 'application/json',
+			},
+			body : JSON.stringify(data)
+		})
+		.then(response => response.json())
+		.then(json => {
+			localStorage.setItem('token', json.token);
+			this.setState({
+				logged_in : true,
+				username : json.user.username
+			})
+		})
+		.catch(error => {
+			console.log(error)
+		})
+		this.setState({
+			displayed_form : ''
+		})
+	}
+	render() {
+		const { logged_in, username, displayed_form } = this.state;
+		return (
+			<div>
+				<NavComponent
+				logged_in = {logged_in}
+				handleLogin = {this.handleLogin}
+				handleLoginChange = {this.handleLoginChange}
+				handleLogout = {this.handleLogout}
+				username = {username}
+				displayed_form = {displayed_form}
+				display_form = {this.display_form}
+				 />
+				<h3>{
+					this.state.logged_in
+					? `Hello ${this.state.username}`
+					: 'Please log in'
+				}</h3>
+			</div>
+		)
+	}
 }
 
-export default App
+export default App;
