f8bbecd930e405e6023ca2347a556c4160edd51d
muslumcanozata <mcan.ozata@gmail.com>
2021-07-13 22:17:02 +0300

    edited stock data

--- src/components/papers/StokTakip.js
+++ src/components/papers/StokTakip.js
@@ -58,37 +58,26 @@ const StokTakip = () => {
 
 	const columns = [
 		{ field: 'id', headerName: 'ID', width: 70 },
-		{ field: 'firstName', headerName: 'First name', width: 130 },
-		{ field: 'lastName', headerName: 'Last name', width: 130 },
+		{ field: 'SKU', headerName: 'SKU', width: 130 },
+		{ field: 'stokAdet', headerName: 'Stok Adedi', width: 160 },
 		{
-		  field: 'age',
-		  headerName: 'Age',
-		  type: 'number',
-		  width: 90,
-		},
-		{
-			field: 'fullName',
-			headerName: 'Full name',
+			field: 'urunAdi',
+			headerName: 'Ürün Adı',
 			description: 'This column has a value getter and is not sortable.',
-			sortable: false,
 			width: 160,
-			valueGetter: (params) =>
-				`${params.getValue(params.id, 'firstName') || ''} ${
-				params.getValue(params.id, 'lastName') || ''
-				}`,
 		},
 	];
 
 	const rows = [
-		{ id: 1, lastName: 'Snow', firstName: 'Jon', age: 35 },
-		{ id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42 },
-		{ id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45 },
-		{ id: 4, lastName: 'Stark', firstName: 'Arya', age: 16 },
-		{ id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
-		{ id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
-		{ id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
-		{ id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
-		{ id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
+		{ id: 1, SKU: 11111, stokAdet: 10, urunAdi: 'Mont Small' },
+		{ id: 2, SKU: 22222, stokAdet: 20, urunAdi: 'Mont Medium' },
+		{ id: 3, SKU: 22223, stokAdet: 30, urunAdi: 'Mont Large' },
+		{ id: 4, SKU: 22224, stokAdet: 40, urunAdi: 'Mont XL' },
+		{ id: 5, SKU: 22225, stokAdet: 50, urunAdi: 'Mont XXL' },
+		{ id: 6, SKU: 22226, stokAdet: 60, urunAdi: 'Mont XXXL' },
+		{ id: 7, SKU: 22227, stokAdet: 70, urunAdi: 'T-Shirt Small' },
+		{ id: 8, SKU: 22228, stokAdet: 80, urunAdi: 'T-Shirt Medium' },
+		{ id: 9, SKU: 22229, stokAdet: 90, urunAdi: 'T-Shirt Large' },
 	];
 
     return (
