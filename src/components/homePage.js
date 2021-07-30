9d4d47b3dbb879ac1eebb1aecafe27ccd9e92da7
simgesismann <62858275+simgesismann@users.noreply.github.com>
2021-07-31 00:06:45 +0300

    minorchanges3107

--- src/components/homePage.js
+++ src/components/homePage.js
@@ -8,8 +8,8 @@ import Box from '@material-ui/core/Box';
 import Container from '@material-ui/core/Container';
 import Grid from '@material-ui/core/Grid';
 import Paper from '@material-ui/core/Paper';
-import Chart from './papers/homepage/Chart';
-import Deposits from './papers/homepage/Deposits';
+import Chart from './papers/homePage/Chart';
+import Deposits from './papers/homePage/Deposits';
 
 const useStyles = makeStyles((theme) => ({
 	root: {
